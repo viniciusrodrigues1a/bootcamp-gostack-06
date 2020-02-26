e759323ed0709536ea0cccc89806b7a2d82e32e0
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-26 16:22:41 -0300

    :sparkles: fazendo chamada a api do github

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User() {
-  return <View />;
-}
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  state = {
+    starredRepos: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ starredRepos: response.data });
+  }
+
+  render() {
+    const { starredRepos } = this.state;
+
+    return <View />;
+  }
