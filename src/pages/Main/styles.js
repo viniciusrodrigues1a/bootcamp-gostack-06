063afffee5249dc49750c1e60c6b31c0d6f18529
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-26 17:30:09 -0300

    :lipstick: removendo padding-bottom do Container

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,7 +3,7 @@ import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 30px;
+  padding: 15px 30px 0;
 `;
 
 export const Form = styled.View`
